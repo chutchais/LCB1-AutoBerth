5a8231358edd00a84fc71027839f333acc6ff52c
chutchais <chutchai.s@gmail.com>
2021-06-17 09:27:14 +0700

    Add script to upload master data

--- src/docker-entrypoint.sh
+++ src/docker-entrypoint.sh
@@ -5,4 +5,9 @@ echo "Start migrations..."
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
 
+
+python manage.py loaddata master_data/group.json
+python manage.py loaddata master_data/user.json
+python manage.py loaddata master_data/berth.json
+
 exec "$@"
